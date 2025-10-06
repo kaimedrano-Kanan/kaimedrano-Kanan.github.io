914b28e7401b3a8780074e1e20169fbcaf2d2edd
kaimedrano-Kanan <kai.medrano@inspire35.org>
2025-10-06 20:26:03 +0000

    kaiwwwwwwwqqqqaazzzz

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,31 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+    createPlatform(0, 200, 200, 50);
+    createPlatform(250, 300, 300, 50);
+    createPlatform(750, 400, 500, 50);
+    createPlatform(450, 500, 200, 50);
+    createPlatform(750, 700, 200, 50);
 
 
 
     // TODO 3 - Create Collectables
-
+    createCollectable("database", 600, 450, 6, 0.87);
+    createCollectable("database", 850, 450, 3, 0.35);
+    createCollectable("database", 400, 250);
+    createCollectable("database", 500, 80, 80, 0.50);
 
 
     
     // TODO 4 - Create Cannons
-
+    createCannon("top", 450, 1500);
+    createCannon("bottom", 1050, 1000);
+    createCannon("left", 300, 1000);
+    createCannon("right", 150, 1500);
 
     
     
